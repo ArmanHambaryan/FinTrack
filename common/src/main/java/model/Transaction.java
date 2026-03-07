d7e9bb8075ab746a2166b1ad05ca9b824d120aae
Arman Hambaryangit config --global user.email armanhambaryan514@gmail.com <armanhambaryan514@gmail.com>
2026-03-07 18:06:32 +0400

    update

---
+++ common/src/main/java/model/Transaction.java
@@ -0,0 +1,35 @@
+package model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "transactions")
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "user_id", nullable = false)
+    private Integer userId;
+
+    private Double amount;
+    private String type;
+    private LocalDateTime transaction_date;
+    private String description;
+    private LocalDateTime created_at;
+    private LocalDateTime updated_at;
+}
---
+++ common/src/main/java/repository/TransactionRepository.java
@@ -0,0 +1,13 @@
+package repository;
+
+import model.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Integer> {
+
+    List<Transaction> findByUserId(Integer userId);
+
+    List<Transaction> findByType(String type);
+}
